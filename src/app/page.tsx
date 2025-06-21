2942a6424dd1d8a63e5bf7d7178b0438ceddab6f
Nagarampalli Sarvan Kumar <sarvankumarnagarampalli478@gmail.com>
2025-06-21 15:09:56 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,7 +17,6 @@ export default function Home() {
       <ProjectsSection />
       <GithubHeatmap />
       <TapeSection/>
-      <Skills/>
       <AboutSection/>
       <Certifications/>
       <Contact/>
